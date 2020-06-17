4aa6549ffde812e9144a47b634307c5c63580066
nathangong <nathangong1234@gmail.com>
2020-06-16 17:07:28 -0700

    Add more features to login and sign up pages

--- lib/pages/create_account_page.dart
+++ lib/pages/create_account_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/components/login_page_buttons.dart';
-import 'package:flutter_app/components/no_action_alert.dart';
-import 'package:flutter_app/constants.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+
+import '../components/login_page_buttons.dart';
+import '../components/no_action_alert.dart';
+import '../constants.dart';
 
 class Account extends StatefulWidget {
   @override
@@ -11,93 +13,106 @@ class Account extends StatefulWidget {
 class _AccountState extends State<Account> {
   String name, email, password, reEnteredPassword;
   TextEditingController reEnteredPasswordController = TextEditingController();
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            gradient: blueGradient,
-          ),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 40.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  alignment: Alignment.topLeft,
-                  child: Text('Create account',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.bold,
+    return ModalProgressHUD(
+      inAsyncCall: loading,
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              gradient: blueGradient,
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.topLeft,
+                    child: Text('Create account',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 30.0),
-                RoundedTextField(
-                  labelText: 'Preferred Name',
-                  icon: Icons.person,
-                  onChanged: (val) {
-                    name = val;
-                  }
-                ),
-                SizedBox(height: 20.0),
-                RoundedTextField(
-                  labelText: 'Email',
-                  icon: Icons.email,
-                  onChanged: (val) {
-                    email = val;
-                  },
-                ),
-                SizedBox(height: 20.0),
-                RoundedTextField(
-                  labelText: 'Password',
-                  icon: Icons.lock,
-                  onChanged: (val) {
-                    password = val;
-                  },
-                  obscureText: true,
-                ),
-                SizedBox(height: 20.0),
-                RoundedTextField(
-                  controller: reEnteredPasswordController,
-                  labelText: 'Re-enter Password',
-                  icon: Icons.lock,
-                  onChanged: (val) {
-                    reEnteredPassword = val;
-                  },
-                  obscureText: true,
-                ),
-                RoundedButton(
-                  text: 'CREATE ACCOUNT',
-                  onPressed: () async {
-                    if (password != reEnteredPassword) {
-                      showDialog(
-                        context: context,
-                        builder: (_) => NoActionAlert(title: 'The passwords you entered did not match, please try again.')
-                      );
-                      reEnteredPasswordController.clear();
+                  SizedBox(height: 30.0),
+                  RoundedTextField(
+                    labelText: 'Preferred Name',
+                    icon: Icons.person,
+                    onChanged: (val) {
+                      name = val;
                     }
-                    else {
-                      try {
-                        var res = await auth.createUserWithEmailAndPassword(email: email, password: password);
-                        db.collection('users').document(res.user.uid).setData({
-                          'name': name,
-                          'email': email,
-                        });
-                      } catch(e) {
+                  ),
+                  SizedBox(height: 20.0),
+                  RoundedTextField(
+                    labelText: 'Email',
+                    icon: Icons.email,
+                    onChanged: (val) {
+                      email = val;
+                    },
+                  ),
+                  SizedBox(height: 20.0),
+                  RoundedTextField(
+                    labelText: 'Password',
+                    icon: Icons.lock,
+                    onChanged: (val) {
+                      password = val;
+                    },
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 20.0),
+                  RoundedTextField(
+                    controller: reEnteredPasswordController,
+                    labelText: 'Re-enter Password',
+                    icon: Icons.lock,
+                    onChanged: (val) {
+                      reEnteredPassword = val;
+                    },
+                    obscureText: true,
+                  ),
+                  RoundedButton(
+                    text: 'CREATE ACCOUNT',
+                    onPressed: () async {
+                      if (password != reEnteredPassword) {
                         showDialog(
-                            context: context,
-                            builder: (_) => NoActionAlert(title: 'Invalid email')
+                          context: context,
+                          builder: (_) => NoActionAlert(title: 'The passwords you entered did not match, please try again.')
                         );
+                        reEnteredPasswordController.clear();
+                      }
+                      else {
+                        setState(() {
+                          loading = true;
+                        });
+
+                        try {
+                          var res = await auth.createUserWithEmailAndPassword(email: email, password: password);
+                          db.collection('users').document(res.user.uid).setData({
+                            'name': name,
+                            'email': email,
+                          });
+                          Navigator.pushReplacementNamed(context, '/route_selection');
+                        } catch(e) {
+                          showDialog(
+                              context: context,
+                              builder: (_) => NoActionAlert(title: 'Invalid email')
+                          );
+                        }
+
+                        setState(() {
+                          loading = false;
+                        });
                       }
                     }
-                  }
-                ),
-              ]
+                  ),
+                ]
+              ),
             ),
           ),
         ),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 import 'package:flutter_app/components/login_page_buttons.dart';
 
@@ -12,94 +13,109 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String email, password;
+  TextEditingController passwordController = TextEditingController();
   bool rememberMe = false;
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            gradient: blueGradient,
-          ),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 40.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  alignment: Alignment.topLeft,
-                  child: Text('Login',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    ),
-                ),
-                SizedBox(height: 30.0),
-                RoundedTextField(
-                  labelText: 'Email',
-                  icon: Icons.email,
-                  onChanged: (val) {
-                    email = val;
-                  },
-                ),
-                SizedBox(height: 20.0),
-                RoundedTextField(
-                  labelText: 'Password',
-                  icon: Icons.lock,
-                  obscureText: true,
-                  onChanged: (val) {
-                    password = val;
-                  }
-                ),
-                Container(
-                  alignment: Alignment.centerRight,
-                  child: FlatButton(
-                    onPressed: () => print('Forgot Password Button Pressed'),
-                    padding: EdgeInsets.only(right: 0.0),
-                    child: Text(
-                      'Forgot Password?',
-                      style: defaultTextStyle.copyWith(fontWeight: FontWeight.bold)
+    return ModalProgressHUD(
+      inAsyncCall: loading,
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              gradient: blueGradient,
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.topLeft,
+                    child: Text('Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                  ),
+                  SizedBox(height: 30.0),
+                  RoundedTextField(
+                    labelText: 'Email',
+                    icon: Icons.email,
+                    onChanged: (val) {
+                      email = val;
+                    },
+                  ),
+                  SizedBox(height: 20.0),
+                  RoundedTextField(
+                    controller: passwordController,
+                    labelText: 'Password',
+                    icon: Icons.lock,
+                    obscureText: true,
+                    onChanged: (val) {
+                      password = val;
+                    }
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: FlatButton(
+                      onPressed: () => print('Forgot Password Button Pressed'),
+                      padding: EdgeInsets.only(right: 0.0),
+                      child: Text(
+                        'Forgot Password?',
+                        style: defaultTextStyle.copyWith(fontWeight: FontWeight.bold)
+                      ),
                     ),
                   ),
-                ),
-                Row(
-                  children: <Widget>[
-                    Checkbox(
-                      value: rememberMe,
-                      checkColor: Colors.blue,
-                      activeColor: Colors.white,
-                      onChanged: (val) {
-                        setState(() {
-                          rememberMe = val;
-                        });
-                      },
-                    ),
-                    Text(
-                      'Remember Me',
-                      style: defaultTextStyle,
-                    ),
-                  ]
-                ),
-                RoundedButton(
-                  text: 'LOGIN',
-                  onPressed: () async {
-                    try {
-                      var res = await auth.signInWithEmailAndPassword(email: email, password: password);
-                      Navigator.pushNamed(context, '/home');
-                    }
-                    catch(e) {
-                      showDialog(
-                        context: context,
-                        builder: (_) => NoActionAlert(title: 'Invalid email or password')
-                      );
-                    }
-                  },
-                ),
-              ]
+                  Row(
+                    children: <Widget>[
+                      Checkbox(
+                        value: rememberMe,
+                        checkColor: Colors.blue,
+                        activeColor: Colors.white,
+                        onChanged: (val) {
+                          setState(() {
+                            rememberMe = val;
+                          });
+                        },
+                      ),
+                      Text(
+                        'Remember Me',
+                        style: defaultTextStyle,
+                      ),
+                    ]
+                  ),
+                  RoundedButton(
+                    text: 'LOGIN',
+                    onPressed: () async {
+                      setState(() {
+                        loading = true;
+                      });
+
+                      try {
+                        var res = await auth.signInWithEmailAndPassword(email: email, password: password);
+                        Navigator.pushReplacementNamed(context, '/home');
+                      }
+                      catch(e) {
+                        showDialog(
+                          context: context,
+                          builder: (_) => NoActionAlert(title: 'Invalid email or password')
+                        );
+                        passwordController.clear();
+                      }
+
+                      setState(() {
+                        loading = false;
+                      });
+                    },
+                  ),
+                ]
+              ),
             ),
           ),
         ),
