3dffbe1deb597b96483aadfad19261a314b2e0a5
nathanpalamuttam <natepal04@gmail.com>
2020-06-19 17:09:22 -0700

    Create GoogleMaps search

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,6 @@ import 'pages/welcome_page.dart';
 import 'pages/google_maps_routes.dart';
 import './maps_redirect.dart';
 import 'googleautocomplete.dart';
-void main() {
-//  WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
-}
-
-
 import 'pages/shelter_profile.dart';
 import 'pages/volunteer_home_page.dart';
 
@@ -31,42 +25,35 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-
-    void main() => runApp(MyApp());
-
-    class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
     return Provider<User>(
-    create: (context) => User(),
-    child: MaterialApp(
-    theme: ThemeData(
-    primarySwatch: Colors.blue,
-    ),
-    initialRoute: '/welcome_page',
-    routes: {
-    '/login': (context) => LoginPage(),
-    '/profile': (context) => ProfilePage(),
-    '/route_selection': (context) => RouteSelectionPage(),
-    '/volunteer': (context) => VolunteerRoute(),
-    '/shelter': (context) => ShelterRoute(),
-    '/signup': (context) => SignUpPage(),
-    '/create_account': (context) => Account(),
-    '/home': (context) => HomePage(),
-    '/settings': (context) => SettingsPage(),
-    '/splash_screen': (context) => SplashScreen(),
-    '/welcome_page': (context) => WelcomePage(),
-    '/google_maps_routes': (context) => GoogleMaps(),
-    '/donate': (context) => DonatePage(),
-    '/volunteer_home': (context) => VolunteerHomePage(),
-    '/shelter_profile': (context) => ShelterProfile(),
-    '/maps_redirect_util': (context) => MapsRedirect(),
-    '/googleautocomplete': (context) => RoutesWidget(),
-    },
-    ),
-    );
+      create: (context) => User(),
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/welcome_page',
+        routes: {
+          '/login': (context) => LoginPage(),
+          '/profile': (context) => ProfilePage(),
+          '/route_selection': (context) => RouteSelectionPage(),
+          '/volunteer': (context) => VolunteerRoute(),
+          '/shelter': (context) => ShelterRoute(),
+          '/signup': (context) => SignUpPage(),
+          '/create_account': (context) => Account(),
+          '/home': (context) => HomePage(),
+          '/settings': (context) => SettingsPage(),
+          '/splash_screen': (context) => SplashScreen(),
+          '/welcome_page': (context) => WelcomePage(),
+          '/google_maps_routes': (context) => GoogleMaps(),
+          '/donate': (context) => DonatePage(),
+          '/volunteer_home': (context) => VolunteerHomePage(),
+          '/shelter_profile': (context) => ShelterProfile(),
+            '/maps_redirect_util': (context) => MapsRedirect(),
+            '/googleautocomplete': (context) => RoutesWidget(),
+          },
+        ),
+      );
     }
-    }
+  }
 
 
