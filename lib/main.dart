8ed92c1468ef72ed36baeb7d58eb7a804607277a
Nathan Kang <nathankang413@gmail.com>
2020-06-19 17:20:08 -0700

    Clean main.dart and folder organization

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'components/google_autocomplete.dart';
 import 'models/user.dart';
 import 'pages/create_account_page.dart';
 import 'pages/donate_page.dart';
+import 'pages/google_maps_routes.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
 import 'pages/profile_page.dart';
 import 'pages/route_selection_page.dart';
 import 'pages/settings.dart';
+import 'pages/shelter_profile.dart';
 import 'pages/shelter_route.dart';
 import 'pages/sign_up_page.dart';
 import 'pages/splash_screen.dart';
+import 'pages/volunteer_home_page.dart';
 import 'pages/volunteer_route.dart';
 import 'pages/welcome_page.dart';
-import 'pages/google_maps_routes.dart';
-import './maps_redirect.dart';
-import 'googleautocomplete.dart';
-import 'pages/shelter_profile.dart';
-import 'pages/volunteer_home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -31,8 +30,8 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        initialRoute: '/welcome_page',
-        routes: {
+        initialRoute: '/welcome',
+        routes: { // TODO: Alphabetize
           '/login': (context) => LoginPage(),
           '/profile': (context) => ProfilePage(),
           '/route_selection': (context) => RouteSelectionPage(),
@@ -43,17 +42,14 @@ class MyApp extends StatelessWidget {
           '/home': (context) => HomePage(),
           '/settings': (context) => SettingsPage(),
           '/splash_screen': (context) => SplashScreen(),
-          '/welcome_page': (context) => WelcomePage(),
+          '/welcome': (context) => WelcomePage(),
           '/google_maps_routes': (context) => GoogleMaps(),
           '/donate': (context) => DonatePage(),
           '/volunteer_home': (context) => VolunteerHomePage(),
           '/shelter_profile': (context) => ShelterProfile(),
-            '/maps_redirect_util': (context) => MapsRedirect(),
-            '/googleautocomplete': (context) => RoutesWidget(),
-          },
-        ),
-      );
-    }
+          '/googleautocomplete': (context) => RoutesWidget(),
+        },
+      ),
+    );
   }
-
-
+}
